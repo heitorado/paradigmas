de64ac737641e824ea92651d6537cff6a4732dcd
Heitorado <hscs@cin.ufpe.br>
2019-04-01 23:32:07 -0300

    Define função toList

--- Códigos de Exercícios/Aula 6/respAula6.hs
+++ Códigos de Exercícios/Aula 6/respAula6.hs
@@ -2,13 +2,18 @@ data Expr = Lit Int |
             Add Expr Expr |
             Sub Expr Expr
 
+data List t = Nil | Cons t (List t)
+
 showExpr :: Expr -> String
 showExpr (Lit i) = show i
 showExpr (Add a b) = "(" ++ showExpr(a) ++ " + " ++ showExpr(b) ++ ")"
 showExpr (Sub a b) = "(" ++ showExpr(a) ++ " - " ++ showExpr(b) ++ ")"
 
 
--- toList :: List t -> [t]
+toList :: List t -> [t]
+toList Nil = []
+toList (Cons t l) = [t] ++ toList(l)
+
 -- fromList :: [t] -> List t
 -- depth :: Tree t -> Int
 -- collapse :: Tree t -> [t]
